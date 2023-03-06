6eff9c71ef93d2bb01f4ad1bc9ba68f7d3e2023b
olkry <adenos90@gmail.com>
2023-03-06 18:19:40 +0300

    Add Lection007

---
+++ Lection007/Program.cs
@@ -0,0 +1,33 @@
+﻿// // Метод с рекурсией, который собирает слова из заданных символов
+// int n = 1;
+// void FindWords(string alphabet, char[] word, int length = 0)
+// {
+//       if (length == word.Length)
+//       {
+//             Console.WriteLine($"{n++} {new String(word)}"); return;
+//       }
+//       for (int i = 0; i < alphabet.Length; i++)
+//       {
+//             word[length] = alphabet[i];
+//             FindWords(alphabet, word, length + 1);
+//       }
+// }
+// FindWords("аисв", new char[4]);
+
+// //Работ с коталогом.Дата создания.
+// string path = "C:/Учёба/3. Знакомство с языками/C#_Lection/C_Sharp_Lection";
+// DirectoryInfo di = new DirectoryInfo(path);
+// Console.WriteLine(di.CreationTime);
+
+// //Метод.Игра в пирамидки
+// int n = 1;
+// void Towers(string with = "1", string on = "3", string some = "2", int count = 5)
+// {
+//  if (count > 1) Towers(with, some, on, count - 1);
+//  Console.WriteLine($"{n++}. {with} >> {on}");
+//  if (count > 1) Towers(some, on, with, count - 1);
+// }
+
+// Towers();
+
+
